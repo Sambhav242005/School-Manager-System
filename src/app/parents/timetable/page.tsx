227bd9ffbf57cff6200e77ffd2c72d5f44661fda
Deepisha1305 <139334997+Deepisha1305@users.noreply.github.com>
2023-07-24 17:08:51 +0530

    ,,,,,,,,

---
+++ src/app/parents/timetable/page.tsx
@@ -0,0 +1,42 @@
+import TimeTable from "@/components/time-table";
+
+export default function(){
+    return<>
+  
+    <header className="w-full bg-gray-800 p-4 flex justify-between items-center">
+  <nav className="flex items-center">
+    <div className="text-white text-xs hidden sm:block ">
+      <a href="#" className="bg-gray-900 hover:bg-gray-700 p-2 rounded cursor-pointer ml-2">Assignments</a>
+      <a href="#" className="bg-gray-900 hover:bg-gray-700 p-2 rounded cursor-pointer ml-5">Scheduled Classes</a>
+      <a href="#" className="bg-gray-900 hover:bg-gray-700 p-2 rounded cursor-pointer ml-5">Query</a>
+    </div>
+  </nav>
+  
+</header>
+
+
+<main className="flex w-full h-screen">
+<aside className="w-80 h-screen bg-gray shadow-md w-fulll hidden sm:block">
+  <div className="flex flex-col justify-between h-screen p-4 bg-gray-800">
+      <div className="text-sm">
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">
+          Time Table</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Test Done</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Upcoming Test</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Test Score</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Report Card</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">PTM</div>
+        <div className="bg-gray-900 text-white p-2 rounded mt-2 cursor-pointer hover:bg-gray-700 hover:text-blue-300">Upcoming Events</div>
+      </div>
+
+
+  </div>
+</aside>
+
+  
+<TimeTable></TimeTable>
+
+
+</main>
+    </>
+}
